b9c035f6f8ccc02c8b1126a6833edc10f4f48c09
domingosneto03 <domingos12126@gmail.com>
2022-10-07 15:31:20 +0100

    Create Hero class

---
+++ src/main/java/Hero.java
@@ -0,0 +1,10 @@
+public class Hero {
+    private int x;
+    private int y;
+
+    public Hero(int x, int y){
+        this.x = x;
+        this.y = y;
+        Hero hero = new Hero(10,10);
+    }
+}
