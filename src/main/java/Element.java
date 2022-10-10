c6b999b39e50aa36a47efa613781d5271da7840a
domingosneto03 <domingos12126@gmail.com>
2022-10-10 16:53:48 +0100

    Created Element

---
+++ src/main/java/Element.java
@@ -0,0 +1,22 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+abstract class Element {
+    private Position position;
+
+    public Element(int x, int y){
+        this.position = new Position(x,y);
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    abstract void draw(TextGraphics graphics);
+}
